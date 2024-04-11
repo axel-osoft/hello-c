5d61563cc78c69ed091cc59dac323971c1632f8b
Alexandre Odoux <alexandre.odoux@sonarsource.com>
2024-04-11 11:39:39 +0200

    Commit nb2

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, Super Great Brave Extraordinary Awesome New World!");
-   printf("Hello, Super Great Brave Extraordinary Awesome New World!");
-   printf("Hello, Super Great Brave Extraordinary Awesome New World!");
-   printf("Hello, Super Great Brave Extraordinary Awesome New World!");
+   printf("Hello, Brave New World!");
    return 0;
 }
